7722724c02b614d4f5288085fe298276431bc4aa
Brandon Le <thesamuraiwho@gmail.com>
2021-08-01 03:56:28 -0700

    Working on fixing settings window.

--- main.py
+++ main.py
@@ -17,12 +17,74 @@ import time
 # TODO Feat: Set increments via a input box
 # TODO Bug: Fix timer element when selecting new images should reset rather than continue?
 
+SETTINGS_FILE = os.path.join(os.path.dirname(__file__), r'settings_file.cfg')
+DEFAULT_SETTINGS = {'max_users': 10, 'user_data_folder': None , 'theme': sg.theme(), 'zipcode' : '94102'}
+# "Map" from the settings dictionary keys to the window's element keys
+SETTINGS_KEYS_TO_ELEMENT_KEYS = {'max_users': '-MAX USERS-', 'user_data_folder': '-USER FOLDER-' , 'theme': '-THEME-', 'zipcode' : '-ZIPCODE-'}
+
+##################### Load/Save Settings File #####################
+def load_settings(settings_file, default_settings):
+    try:
+        with open(settings_file, 'r') as f:
+            settings = jsonload(f)
+    except Exception as e:
+        sg.popup_quick_message(f'exception {e}', 'No settings file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+        settings = default_settings
+        save_settings(settings_file, settings, None)
+    return settings
+
+
+def save_settings(settings_file, settings, values):
+    if values:      # if there are stuff specified by another window, fill in those values
+        for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:  # update window with the values read from settings file
+            try:
+                settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
+            except Exception as e:
+                print(f'Problem updating settings from window values. Key = {key}')
+
+    with open(settings_file, 'w') as f:
+        jsondump(settings, f)
+
+    sg.popup('Settings saved')
+
+##################### Make a settings window #####################
+def create_settings_window(settings):
+    sg.theme(settings['theme'])
+
+    def TextLabel(text): return sg.Text(text+':', justification='r', size=(15,1))
+
+    layout = [  [sg.Text('Settings', font='Any 15')],
+                [TextLabel('Max Users'), sg.Input(key='-MAX USERS-')],
+                [TextLabel('User Folder'),sg.Input(key='-USER FOLDER-'), sg.FolderBrowse(target='-USER FOLDER-')],
+                [TextLabel('Zipcode'),sg.Input(key='-ZIPCODE-')],
+                [TextLabel('Theme'),sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
+                [sg.Button('Save'), sg.Button('Exit')]  ]
+
+    window = sg.Window('Settings', layout, keep_on_top=True, finalize=True)
+
+    for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:   # update window with the values read from settings file
+        try:
+            window[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]].update(value=settings[key])
+        except Exception as e:
+            print(f'Problem updating PySimpleGUI window from settings. Key = {key}')
+
+    return window
+
+##################### Main Program Window & Event Loop #####################
+def create_main_window(settings):
+    sg.theme(settings['theme'])
+
+    layout = [[sg.T('This is my main application')],
+              [sg.T('Add your primary window stuff in here')],
+              [sg.B('Ok'), sg.B('Exit'), sg.B('Change Settings')]]
+
+    return sg.Window('Main Application', layout)
+
 def Radio(key, text, group_id, default=False): return sg.Radio(key=key, text=text, group_id=group_id, 
     default=default, enable_events=True)
 
 def time_as_int():
     return int(round(time.time() * 100))
-
 
 def get_img_data(f, maxsize=(1200, 850), first=False):
     """Generate image data using PIL
@@ -69,7 +131,7 @@ def get_img_data(f, maxsize=(1200, 850), first=False):
     return ImageTk.PhotoImage(img)
 
 
-if __name__ == "__main__":
+def main():
     current_time, paused_time, paused = 0, 0, False
     start_time = time_as_int()
 
@@ -95,7 +157,6 @@ if __name__ == "__main__":
         [sg.HSeparator()],
         ]
     
-
     window = sg.Window('Session Settings', session_layout, keep_on_top=True, finalize=True)
 
     # Get the folder containing the images from the user
@@ -152,18 +213,25 @@ if __name__ == "__main__":
                     sg.Button(key='-INC-TIMER-', button_text='+', button_color=('white', '#FFA500'), size=(8, 2))],
                  [sg.Button(key='-RUN-PAUSE-', button_text='Pause', button_color=('white', '#001480'), size=(8, 2)),
                     sg.Button(key='-RESET-', button_text='Reset', button_color=('white', '#007339'), size=(8, 2)),
-                    sg.Exit(key='-EXIT-', button_text='Exit', button_color=('white', 'firebrick4'), size=(8, 2))]]
+                    sg.Exit(key='-EXIT-', button_text='Exit', button_color=('white', 'firebrick4'), size=(8, 2)),
+                    sg.B('Change Settings')]]
 
     layout = [[sg.Column(vertical_alignment='top', layout=col_files), sg.Column(vertical_alignment='top', layout=col)]]
 
-    window = sg.Window('Simple Gesture Show', layout, return_keyboard_events=True,
-                       location=(0, 0), size=(1920, 1080), background_color='#272927',
-                       resizable=True, use_default_focus=False)
+    # window = sg.Window('Simple Gesture Show', layout, return_keyboard_events=True,
+    #                    location=(0, 0), size=(1920, 1080), background_color='#272927',
+    #                    resizable=True, use_default_focus=False)
+
+    window, settings = None, load_settings(SETTINGS_FILE, DEFAULT_SETTINGS)
 
     # loop reading the user input and displaying image, filename
     NEXTIMGTIMEOUT = 6000 #1000
     i = 0
     while True:
+
+        if window is None:
+            window = create_main_window(settings)
+
         # print(sg.Window.get_screen_size())
         # print(paused)
         if not paused:
@@ -177,6 +245,12 @@ if __name__ == "__main__":
 
         if event in (sg.WIN_CLOSED, '-EXIT-'):
             break
+        elif event == 'Change Settings':
+            event, values = create_settings_window(settings).read(close=True)
+            if event == 'Save':
+                window.close()
+                window = None
+                save_settings(SETTINGS_FILE, settings, values)
         elif event == '-DEC-TIMER-':
             if NEXTIMGTIMEOUT > 6000:
                 NEXTIMGTIMEOUT -= 6000
@@ -253,3 +327,6 @@ if __name__ == "__main__":
         window.Refresh()
 
     window.close()
+
+if __name__ == "__main__":
+    main()
