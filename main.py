2123de5fa29b00ddf5fa21e90fabc7a766392c99
Brandon Le <thesamuraiwho@gmail.com>
2021-08-06 02:37:56 -0700

    Adding stopwatch functionality. Issue with resetting time cutting off the first second after reset so makes the stopwatch inaccurate there.

--- main.py
+++ main.py
@@ -22,11 +22,16 @@ from enum import Enum
 # TODO Feat: Aave thumbnails in temporary directory and delete this directory when program exits
 #   to avoid redoing image processing work
 
+# Constants
+SEC = 1000 # 1000ms == 1 sec
+MIN = 60000 # 60000ms == 1 min
+MODES_KEYS = ["-SESSION_CONST_MODE-", "-SESSION_CLASS_MODE-", "-SESSION_CUSTM_MODE-"]
+MODES_VALUES = ["-LAYOUT_CONST-", "-LAYOUT_CLASS-", "-LAYOUT_CUSTM-"]
+
 class Session_Mode(Enum):
     CONSTANT = 0
     CLASS = 1
     CUSTOM = 2
-
 
 def Radio(key, text, group_id, default=False): return sg.Radio(key=key, text=text, group_id=group_id, 
     default=default, enable_events=True)
@@ -78,23 +83,26 @@ def get_img_data(f, maxsize=(1200, 850), first=False):
 
 def main():
     sg.theme('lightgreen6')
-    current_time, paused_time, paused = 0, 0, True
     session_mode = Session_Mode.CONSTANT
+    next_timeout = 1000 # 100 = 1 seconds
+    current_time, time_diff, paused = 0, next_timeout, True
     start_time = time_as_int()
+    end_time = 0
     folder = ""
     fnames = []
     file_num_display_elem = sg.Text(key='-FILE_NUM-', text='', size=(15,1))
     image_elem = sg.Image(key='-IMAGE_ELEM-', data=None)
     col = [[image_elem]]
     num_files = 0
+    
 
     folder_browser = [[sg.FolderBrowse(button_text="Browse",
         initial_folder=os.getcwd(),
         enable_events=True,
-        font=('Helvetica', 32),
+        font=('Helvetica', 20),
         key="-FOLDER_BROWSER-")]]
 
-    files = [[sg.Listbox(key='-LISTBOX-', values=fnames, change_submits=True, size=(50, 20))],
+    files = [[sg.Listbox(key='-LISTBOX-', values=fnames, change_submits=True, size=(50, 10))],
         [sg.Button(key='-PREV-', button_text='Prev', size=(8, 2)), 
             sg.Button(key='-NEXT-', button_text='Next', size=(8, 2)), file_num_display_elem]]
 
@@ -136,25 +144,20 @@ def main():
     layout_class = [[sg.Column(class_mode, key="-LAYOUT_CLASS-", visible=False)]]
     layout_custm = [[sg.Column(custm_mode, key="-LAYOUT_CUSTM-", visible=False)]]
     layout = [[folder_browser], [sg.Column(key='-CONTROLS-', vertical_alignment='top', layout=files + session 
-        + layout_const + layout_class + layout_custm + timer_layout), sg.Column(key='-IMAGE-', vertical_alignment='top', layout=col)]]
+        + layout_const + layout_class + layout_custm + timer_layout), 
+        sg.Column(key='-IMAGE-', vertical_alignment='top', layout=col, visible=False)]]
 
     # loop reading the user input and displaying image, filename
-    NEXTIMGTIMEOUT = 6000 # 1 minute or 6000 ms
-    i = 0
-    MODES_KEYS = ["-SESSION_CONST_MODE-", "-SESSION_CLASS_MODE-", "-SESSION_CUSTM_MODE-"]
-    MODES_VALUES = ["-LAYOUT_CONST-", "-LAYOUT_CLASS-", "-LAYOUT_CUSTM-"]
+    img = 0
 
     window = sg.Window('Simple Gesture Show', layout, return_keyboard_events=True,
                        location=(0, 0), size=(1920, 1080), background_color='#272927',
                        resizable=True, use_default_focus=False)
 
     while True:
-        # if window is None:
-        #     window = create_main_window(settings)
-
         # print(sg.Window.get_screen_size())
         # print(paused)
-        event, values = window.read()
+        event, values = window.read(timeout=15)
         print(f"event: {event}\nvalues: {values}\n")
 
         if event in (sg.WIN_CLOSED, '-EXIT-'):
@@ -208,6 +211,8 @@ def main():
             #     sg.Button(key='-NEXT-', button_text='Next', size=(8, 2)), file_num_display_elem],
             #     [sg.Text('')]]
             
+            # window['-CONTROLS-'].update(visible=True)
+            window['-IMAGE-'].update(visible=True)
             window['-LISTBOX-'].update(values=fnames)
             window['-FILE_NUM-'].update(f'File 1 of {num_files}')
             window['-IMAGE_ELEM-'].update(data=get_img_data(filename, first=True))
@@ -215,16 +220,13 @@ def main():
             # window['-CONTROLS-'].update(layout=folder_browser + files + session + timer_layout)
 
         elif folder:
-            if not paused:
-                # event, values = window.read(timeout=10)
-                current_time = time_as_int() - start_time
-                print(f'values["-TIMER-VALUE-"]: {values["-TIMER-VALUE-"]}')
+            print(f'start_time:{start_time}\ncurrent_time: {current_time}\nend_time: {end_time}\ntime_diff: {time_diff}\n')
 
             if event in MODES_KEYS:
-                for i in range(len(MODES_KEYS)):
-                    if values[MODES_KEYS[i]]:
-                        session_mode = Session_Mode(i)
-                        new_layout = MODES_VALUES[i]
+                for choice in range(len(MODES_KEYS)):
+                    if values[MODES_KEYS[choice]]:
+                        session_mode = Session_Mode(choice)
+                        new_layout = MODES_VALUES[choice]
                         for lay in MODES_VALUES:
                             window[lay].update(visible=False)
                         window[new_layout].update(visible=True)
@@ -237,76 +239,103 @@ def main():
                 pass
             elif session_mode == Session_Mode.CUSTOM:
                 if event == '-DEC-TIMER-':
-                    if NEXTIMGTIMEOUT > 6000:
-                        NEXTIMGTIMEOUT -= 6000
-                        if NEXTIMGTIMEOUT // 6000 > 1:
+                    if next_timeout > 6000:
+                        next_timeout -= 6000
+                        if next_timeout // 6000 > 1:
                             s = 's'
                         else:
                             s = ''
                         # TODO fix current timeout display
-                        new_text = f"Current timeout: {NEXTIMGTIMEOUT // 6000} Minute{s}"
+                        new_text = f"Current timeout: {next_timeout // 6000} Minute{s}"
                         print(new_text)
                         window['-CUR-TIMEOUT-'].update(value=new_text)
                 elif event == '-INC-TIMER-':
-                    if NEXTIMGTIMEOUT < 6000 * 60:
-                        NEXTIMGTIMEOUT += 6000
-                        if NEXTIMGTIMEOUT // 6000 > 1:
+                    if next_timeout < 6000 * 60:
+                        next_timeout += 6000
+                        if next_timeout // 6000 > 1:
                             s = 's'
                         else:
                             s = ''
 
                         # TODO fix current timeout display
-                        new_text = f"Current timeout: {NEXTIMGTIMEOUT // 6000} Minute{s}"
+                        new_text = f"Current timeout: {next_timeout // 6000} Minute{s}"
                         print(new_text)
                         window['-CUR-TIMEOUT-'].update(value=new_text)
+
             if event == '-RESET-':
-                paused_time = start_time = time_as_int()
-                current_time = 0
+                current_time = start_time = time_as_int()
+                end_time = current_time + next_timeout
+                time_diff = next_timeout
+                print("pressed reset button")
             elif event == '-RUN-PAUSE-':
+                print("pressed run-pause button")
                 paused = not paused
+                print(f"paused: {paused}")
                 if paused:
-                    paused_time = time_as_int()
+                    time_diff = end_time - current_time
                 else:
-                    start_time = start_time + time_as_int() - paused_time
+                    start_time = time_as_int() 
+                    current_time = start_time
+                    end_time = start_time + time_diff
+                    print(f'start_time:{start_time}\ncurrent_time: {current_time}\nend_time: {end_time}\ntime_diff: {time_diff}\n')
                 # Change button's text
                 window['-RUN-PAUSE-'].update('Run' if paused else 'Pause')
-            if event in ('-NEXT-', 'Down:40', 'Next:34'):
-                i += 1
-                if i >= num_files:
-                    i -= num_files
-                filename = os.path.join(folder, fnames[i])
+
+            if event == '-NEXT-': #in ('-NEXT-', 'Down:40', 'Next:34'):
+                img += 1
+                if img >= num_files:
+                    img -= num_files
+                filename = os.path.join(folder, fnames[img])
                 current_time = 0
-            elif event in ('-PREV-', 'Up:38', 'Prior:33'):
-                i -= 1
-                if i < 0:
-                    i = num_files + i
-                filename = os.path.join(folder, fnames[i])
+            elif event == '-PREV-': #in ('-PREV-', 'Up:38', 'Prior:33'):
+                img -= 1
+                if img < 0:
+                    img += num_files
+                filename = os.path.join(folder, fnames[img])
                 current_time = 0
             elif event == '-LISTBOX-':              # something from the listbox
                 f = values['-LISTBOX-'][0]          # selected filename
                 filename = os.path.join(folder, f)  # read this file
-                i = fnames.index(f)                 # update running index
+                img = fnames.index(f)                 # update running index
             else:
-                filename = os.path.join(folder, fnames[i])
-
-            if current_time > NEXTIMGTIMEOUT:
+                filename = os.path.join(folder, fnames[img])
+            
+            print(f'start_time:{start_time}\ncurrent_time: {current_time}\nend_time: {end_time}\ntime_diff: {time_diff}\n')
+            if time_diff <= 50 and not paused:
                 print("\n\n\nRESET\n\n\n")
-                paused_time = start_time = time_as_int()
-                current_time = 0
-                i += 1
-                if i >= num_files:
-                    i %= num_files
-                filename = os.path.join(folder, fnames[i])
+                current_time = start_time = time_as_int()
+                end_time = current_time + next_timeout
+                time_diff = next_timeout
+                img += 1
+                if img >= num_files:
+                    img %= num_files
+                filename = os.path.join(folder, fnames[img])
+            elif time_diff > 0 and not paused:
+                print("\n\n\nTIMER RUNNING\n\n\n")
+                current_time = time_as_int()
+                time_diff = end_time - current_time
+            print(f'start_time:{start_time}\ncurrent_time: {current_time}\nend_time: {end_time}\ntime_diff: {time_diff}\n')
+
+            # if current_time > NEXTIMGTIMEOUT:
+            #     print("\n\n\nRESET\n\n\n")
+            #     paused_time = start_time = time_as_int()
+            #     current_time = 0
+            #     img += 1
+            #     if img >= num_files:
+            #         img %= num_files
+            #     filename = os.path.join(folder, fnames[img])
 
             # --------- Display timer in window --------
-            window['-TIMER-'].update('{:02d}:{:02d}'.format((current_time // 100) // 60,
-                                                        (current_time // 100) % 60))
+            # window['-TIMER-'].update('{:02d}:{:02d}'.format((current_time // 100) // 60,
+            #                                             (current_time // 100) % 60))
+            window['-TIMER-'].update('{:02d}:{:02d}'.format(((time_diff) // 100) // 60,
+                                                        ((time_diff) // 100) % 60))
 
             # update window with new image
             image_elem.update(data=get_img_data(filename, first=True))
             window['-IMAGE_ELEM-'].update(data=get_img_data(filename, first=True))
             # update page display
-            file_num_display_elem.update('File {} of {}'.format(i+1, num_files))
+            # file_num_display_elem.update('File {} of {}'.format(i+1, num_files))
             window.Refresh()
 
     window.close()
